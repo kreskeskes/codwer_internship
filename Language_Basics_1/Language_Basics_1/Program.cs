348259b020425c1eec08435db5176e40375d5d3e
kreskeskes <travojuika@gmail.com>
2023-10-05 18:20:14 +0300

    The 1st assignment from Language Basics
    - I have made a program that reverses a number and does the verification either the number from input is a perfect square or not, using math functions.
    -I restricted so that the input string can consist of digits only by using regular expression

---
+++ Language_Basics_1/Language_Basics_1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Language_Basics_1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+        inputNumber:
+            Console.WriteLine("Please enter a number with at least 3 digits");
+            string numStr = Console.ReadLine();
+            string newNumStr = null;
+            //regular expression to check restrict the input to numbers only
+            Regex regex = new Regex("^[0-9]*$");
+            if (regex.IsMatch(numStr) && numStr.Length >= 3)
+            {
+                //reversing the string and adding the received characters to newNumStr
+                for (int i = numStr.Length - 1; i >= 0; i--)
+                {
+                    newNumStr += numStr[i];
+                }
+                int number = Convert.ToInt32(newNumStr);
+                double squareRoot = Math.Sqrt(number);
+                if (number / squareRoot == squareRoot)
+                {
+                    Console.WriteLine("The number is a perfect square");
+                }
+                else
+                {
+                    Console.WriteLine("The number is not a perfect square");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input, please input a number with at least 3 digits");
+                goto inputNumber;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
