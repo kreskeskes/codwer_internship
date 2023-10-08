0784e6d5aac0bbcfe6f92002416a01e48acfde77
kreskeskes <travojuika@gmail.com>
2023-10-08 21:05:50 +0300

    Program that counts the number of times a letter was used in a word
    I used LINQ in order to perform the count of characters

---
+++ Language_Basics_3/Language_Basics_3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+
+
+        //check if this word has repeating chars
+        Console.WriteLine("Enter the number of words that you'd like to have");
+        int countOfWords = Convert.ToInt32(Console.ReadLine());
+        List<string> words = new List<string>();
+        for (int i = 0; i < countOfWords; i++)
+        {
+            Console.WriteLine("Enter the new word.");
+            string word = Console.ReadLine().ToUpper();
+            words.Add(word);
+        }
+        string unitedString = null;
+        foreach (string word in words)
+        {
+            unitedString += word;
+        }
+        foreach (string s in words)
+        {
+            for (int i = 0; i < s.Length; i++)
+            {
+
+                int occurrences = s.Count(temp => temp == s[i]);
+                //Using LINQ features to get the count of elements that satisfy the lambda expression condition
+                Console.WriteLine($"Occurrences of letter: {s[i]} in the word {s} is {occurrences} times");
+
+            }
+            Console.WriteLine();
+
+        }
+        foreach (char character in unitedString)
+        {
+
+            int occurrencesInTheWhole = unitedString.Count(temp => temp == character);
+            Console.WriteLine($"Occurrences of letter: {character} in all of the names in the string of {unitedString} is {occurrencesInTheWhole} times");
+        }
+
+        Console.ReadKey();
+
+    }
+}
